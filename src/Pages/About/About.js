233ad0f51e84d2c3f80b0e9a5fd2a971d7af6a13
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-04-12 16:26:02 +0600

    Home and about page complated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,91 @@
 import React from 'react';
-
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className='container pt-5 pb-5'>
+            <div className="row">
+                <div className="col-md-3 border-top border-5 border-info  position-relative ">
+                    <div className='d-flex flex-column position-sticky top-0'>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus1">What is art?</a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus2">History of painting </a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus3">Why painting is important?</a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus4">#1 Paintings increase cultural appreciation</a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus5">#2 Paintings help people learn history</a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus6">#3 Painting boosts creativity</a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus7"> #4 Painting can improve your focus </a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus8"> #5 Painting can improve problem-solving skills </a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus9"> #6 Painting helps you communicate emotions </a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus10"> #7 Painting can improve self-esteem </a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus11"> #8 Looking at paintings increases blood flow to the brain </a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus12"> #9 Painting can improve mental health </a>
+                        <a className='text-decoration-none text-dark p-2 border-bottom' href="#qus13"> #10 Painting can improve health in your older years </a>
+                    </div>
+                </div>
+                <div className="col-md-9">
+                        <h5 id='qus1'>What is art?</h5>
+                        <p>
+                            The dictionary definition of art says that it is “the conscious use of skill and creative imagination especially in the production of aesthetic objects” (Merriam-Webster)
+                        </p>
+                        <h5 id='qus2'>History of painting : </h5>
+                        <p>
+                            The history of painting reaches back in time to artifacts and artwork created by pre-historic artists, and spans all cultures. It represents a continuous, though periodically disrupted, tradition from Antiquity. Across cultures, continents, and millennia, the history of painting consists of an ongoing river of creativity that continues into the 21st century.Until the early 20th century it relied primarily on representational, religious and classical motifs, after which time more purely abstract and conceptual approaches gained favor.
+
+                            Developments in Eastern painting historically parallel those in Western painting, in general, a few centuries earlier.African art, Jewish art, Islamic art, Indonesian art, Indian art, Chinese art, and Japanese art each had significant influence on Western art, and vice versa.
+
+                            Initially serving utilitarian purpose, followed by imperial, private, civic, and religious patronage, Eastern and Western painting later found audiences in the aristocracy and the middle class. From the Modern era, the Middle Ages through the Renaissance painters worked for the church and a wealthy aristocracy. Beginning with the Baroque era artists received private commissions from a more educated and prosperous middle class.Finally in the West the idea of "art for art's sake" began to find expression in the work of the Romantic painters like Francisco de Goya, John Constable, and J. M. W. Turner. The 19th century saw the rise of the commercial art gallery, which provided patronage in the 20th century.
+                        </p>
+                        <h5 id='qus3'>Why painting is important?</h5>
+                        <p>
+                            Probably, the best theory that I like all which best explains - Why is art important - is from Van Jones, subtly provides a great response to What is art?
+                            Van Jones presented a graph that accurately represents the interaction between the four aspects of society and its different members.
+                            Consequently, Vones depicts why is art important to our society?
+                            The graph (below) represents our society.
+                            Society is driven by the powerful elites, the dependent masses, government, cultural producers and artists
+                        </p>
+                        <br />
+                        <p><b>Here is the more rezone</b></p>
+                        <p id='qus4'>
+                            <b>#1 Paintings increase cultural appreciation</b><br />
+                            Learning about (and appreciating) different cultures comes with many benefits. It's also part of living in the world as a global citizen. Studying paintings is a good way to develop cultural appreciation, especially for young people who may not have much exposure to other cultures beyond their own. True cultural appreciation goes beyond looking at a painting. It's also important to dig into the context of an art style, who the artist was, and what they were trying to communicate.
+                        </p>
+                        <p id='qus5'>
+                            <b> #2 Paintings help people learn history</b><br />
+                            Art mirrors and comments on the time it was created. Studying paintings from different periods sheds light on the worldviews of the time. Many paintings depict specific events and important people, as well. Studying history through an artist's perspective is different from reading about it in a textbook. For many students, especially visual learners, learning history through art helps them absorb information better.
+                        </p>
+                        <p id='qus6'>
+                            <b> #3 Painting boosts creativity </b><br />
+                            When you paint, you're using both sides of your brain. Experts used to think that people were either “left-brained” or “right-brained,” and that creativity was found on the right side. New research suggests that's not the case. Following a stroke that affected the left side of his brain, an artist struggled to make new art. He eventually regained the motor skills necessary to draw, but while he could copy images, coming up with something original was difficult. Researchers believed this showed that both sides of the brain were involved in creating art. The more you paint, the more you use your entire brain and improve your creative skills.
+                        </p>
+                        <p id='qus7'>
+                            <b> #4 Painting can improve your focus </b><br />
+                            When you paint, you're stimulating the parts of your brain responsible for concentration and memory. For people with ADHD, which includes symptoms like struggling with focus, art therapy can help them stay more engaged. To boost your brain's ability to focus, try painting with as few distractions as possible. If you're studying a painting, focus your brain by looking for focal points, interesting compositions, color patterns, and more.
+                        </p>
+                        <p id='qus8'>
+                            <b> #5 Painting can improve problem-solving skills </b><br />
+                            When you take up painting, you know that skills like creativity and focus will most likely improve. Painting can help you become a better problem-solver, too! Every artist makes mistakes. Recognizing a mistake and figuring out how to fix it exercises the problem-solving part of your brain. Painters are good at adapting and thinking outside the box. The more you paint, the sharper your critical thinking and problem-solving skills become.
+                        </p>
+                        <p id='qus9'>
+                            <b> #6 Painting helps you communicate emotions</b><br />
+                            It can be difficult to communicate your feelings. This is especially true with challenging feelings like anger and grief. Trauma is very difficult to articulate, too. Many psychologists recommend art therapy for their clients. It helps release your feelings without words and puts you in a more regulated, relaxed state. Looking at paintings and learning about artists who struggled with their emotions can help, too. It's cathartic to see representations of emotions on canvas.
+                        </p>
+                        <p id='qus10'>
+                            <b>#7 Painting can improve self-esteem</b><br />
+                            Research indicates that when kids participate in the arts, they enjoy a boost of confidence and self-esteem. They don't even need to be particularly good at painting. One study, published in the Annals of the New York Academy of Sciences, took a look at 6,209 students from the United Kingdom Millennium Cohort Study. Most of the data came from interviews when they were 11-years old. Students who engaged in creative activities most days were more likely to have high self-esteem than students who had fewer opportunities. The reasons why aren't quite clear, but the researchers believed it could be because creating art helps kids form a unique identity and feel accomplished.
+                        </p>
+                        <p id='qus11'>
+                            <b>#8 Looking at paintings increases blood flow to the brain</b><br />
+                            You know that painting stimulates the brain, but just looking at paintings has an effect, too. Years ago, Professor Semir Zeki (chair in neuroaesthetics at University College London) conducted an experiment where he scanned peoples' brains as they looked at a beautiful painting. It didn't matter whether it was a landscape, portrait, or still life. When people looked at art they found beautiful, blood flow to a certain part of the brain increased by as much as 10%. It's similar to what happens when you look at someone you love. Why does this matter? Experiencing pleasurable things, like looking at paintings or going for a walk in a forest, directly impacts a person's well-being.
+                        </p>
+                        <p id='qus12'>
+                            <b>#9 Painting can improve mental health</b><br />
+                            Speaking of well-being, painting is linked to reduced stress and better mental health. That goes against the stereotype of the “tortured artist.” Studies find that painting and other art therapy can play a role in treating mental illnesses. When you stimulate your brain through art, it's easier to relax. This lowers your stress and helps you manage symptoms of anxiety and depression.
+                        </p>
+                        <p id='qus13'>
+                            <b>#10 Painting can improve health in your older years</b><br />
+                            A study from The Mayo Clinic found that painting can help prevent the memory and cognition problems that often pop up later in life. Those who identify as artists are less likely to suffer cognitive impairment than those who don't engage in art at all. Other research shows that people at risk for Alzheimer's and dementia benefit significantly from drawing and painting. The earlier you start, the better the benefits, but it's never too late to try painting.
+                        </p>
+                    
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,9 +3,22 @@ import { Button } from 'react-bootstrap';
 
 const Home = () => {
     return (
-        <div>
-            <h2 className='text-info'>Home</h2>
-            <Button variant="warning">Warning</Button>
+        <div className='container'>
+            <div className='row align-items-center min-vh-100'>
+                <div className='col-md-6' >
+                    <h2 className='text-dark'>
+                        A Painting <span className='text-info'>Explosion</span> our Feeling
+                    </h2>
+                    <p>
+                        A painting is the foundation of a feelings explosion; images and colors that hide inside common perceptions like hope, desire, recovery. It may serve as a sole contemplation medium. Sometimes it may be a better therapy than medicine. The effect becomes stronger when one feels it can pass it to others too. A painting is a gift for everyone. It may be a scan of a destiny and may serve as therapy for the one that finds himself in this creative universe. The importance resides in the strong corelation of “state” and “color” along sent message.
+                    </p>
+                    <div className=' d-flex gap-2'>
+                        <Button variant="info text-light">Learn More</Button>
+                        <Button variant="info text-light">Buy Painting </Button>
+                    </div>
+                </div>
+                <img className='col-md-6' src="https://www.nguyenartgallery.com/wp-content/uploads/Benefits-Of-Painting.jpg" alt="paintingImage" />
+            </div>
         </div>
     );
 };
