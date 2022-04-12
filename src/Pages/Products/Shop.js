c2ba83b7258d68bf41de22d5a31b2f416cbb5858
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-04-12 22:30:56 +0600

    make Custom hoos for search and fatch function

--- src/Pages/Products/Shop.js
+++ src/Pages/Products/Shop.js
@@ -1,62 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import usePaintings from '../../hooks/usePaintings';
 import './Product.css';
 import Products from './Products';
 import SearchProducts from './SearchProducts';
 
 const Shop = () => {
-    const [paintings, setPaintings] = usePaintings();
-  
-    const [searchResults, setSearchResults] = useState([])
-      const handleSearchChange = (event) => {
-      const searchText = event.target.value;
-      if (searchText === ''  ) {
-        setSearchResults([])
-        return;
-      }
-      if (!searchText.trim() ) {
-        return;
-      }
-      console.log(searchText);
-      const match = paintings.filter(paint => paint.name.toLowerCase().includes(searchText.toLowerCase()));
-      setSearchResults(match);
-    }
-    
-  
-    return (
-      <div className='main-search-div'>
-        <div className='productCard_container'>
+  const { paintings, searchResults, handleSearchChange } = usePaintings();
+
+  return (
+    <div className='main-search-div'>
+      <div className='productCard_container'>
+        {
+          paintings.map((painting) => (
+            <Products
+              key={painting.id}
+              paintingData={painting}
+            />
+          ))
+        }
+      </div>
+      <div className='position_sticky'>
+        <div className='search-area'>
+          <h2 >Find your painting </h2>
+          <p> Here Total {paintings.length} paintings </p>
+          <input
+            onChange={handleSearchChange}
+            type="text"
+            style={{ border: '5px solid #aad5e8', padding: '10px 10px', margin: '20px', borderRadius: '7px', width: '80%', fontSize: '20px' }}
+            placeholder='Search' />
+        </div>
+        <div className='search-result-area'>
           {
-            paintings.map((painting) => (
-              <Products
+            searchResults.map((painting) => (
+              <SearchProducts
                 key={painting.id}
                 paintingData={painting}
               />
-            ))
+            )).slice(0, 3)
           }
         </div>
-        <div className='position_sticky'>
-          <div className='search-area'>
-            <h2 >Find your painting from {paintings.length} items</h2>
-            <input
-              onChange={handleSearchChange}
-              type="text"
-              style={{ border: '5px solid #aad5e8', padding: '10px 10px', margin: '20px', borderRadius: '7px', width: '80%', fontSize: '20px' }}
-              placeholder='Search' />
-          </div>
-          <div className='search-result-area'>
-            {
-              searchResults.map((painting) => (
-                <SearchProducts
-                  key={painting.id}
-                  paintingData={painting}
-                />
-              )).slice(0, 3)
-            }
-          </div>
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Shop;
--- src/hooks/usePaintings.js
+++ src/hooks/usePaintings.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 const usePaintings = () => {
     const [paintings, setPaintings] = useState([]);
+    const [searchResults, setSearchResults] = useState([])
+
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/kibria-khandaker/my-api-data/main/fakeData.json')
             .then(res => res.json())
@@ -10,9 +12,22 @@ const usePaintings = () => {
                 // setSearchResults(data)
             })
     }, []);
-    return [paintings, setPaintings];
+
+    const handleSearchChange = (event) => {
+        const searchText = event.target.value;
+        if (searchText === '') {
+            setSearchResults([])
+            return;
+        }
+        if (!searchText.trim()) {
+            return;
+        }
+        console.log(searchText);
+        const match = paintings.filter(paint => paint.name.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchResults(match);
+    }
+
+    return { paintings, setPaintings, searchResults, setSearchResults, handleSearchChange };
 }
 
 export default usePaintings;
-
-// need to use this object for search function
